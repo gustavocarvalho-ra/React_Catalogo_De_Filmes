3b038844c10c2cdb0c9b7760bb038236d4e9fe4b
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-07 19:14:10 -0300

    at6

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,12 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs'
+
 const Movie = () => {
   return <div> Movie</div>
 };
