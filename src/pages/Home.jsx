4562f6038ab3d021998079635a292a7d15612bba
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-07 16:21:55 -0300

    at2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,25 @@
+import { userState, UseEffect, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data)
+  };
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    console.log(topRatedUrl);
+  }, [])
+
   return <div>Home</div>
 };
 
