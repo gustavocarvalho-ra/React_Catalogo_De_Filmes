e19f72bf8e7455f1cafae94f4c16ce8cdac126d2
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-07 18:36:49 -0300

    at5

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,20 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import {FaStar} from 'react-icons/fa'
+import { FaStar } from "react-icons/fa";
 
 const imagesURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
   return (
     <div className="movie-card">
-      <img src="{imagesURL + movie.poster_path}" alt={movie.title} />
+      <img src={imagesURL + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>
-        <FaStar /> {movie.vote_avarage}
+        <FaStar /> {movie.vote_average}
       </p>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   );
 };
 
-export default MovieCard
+export default MovieCard;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,42 @@
-import { Link } from "react-router-dom";
-import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
   return (
-    <nav id='navbar'>
+    <nav id="navbar">
       <h2>
-        <Link to="/"><BiCameraMovie /> LibMovies</Link>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+        </Link>
       </h2>
-      <form>
-        <input type="text" placeholder="Busque um filme" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
       </form>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
